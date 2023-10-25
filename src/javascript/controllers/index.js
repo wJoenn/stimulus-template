6b7e48d6b48a1f5d68a6370130e39b669c2d1acd
Louis Ramos <louisramosdev@gmail.com>
2023-10-25 11:02:49 +0200

    Update eslint config

--- src/javascript/controllers/index.js
+++ src/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/newline-after-import */
-import application from "./application"
+import application from "./application.js"
 
-import CountController from "./count_controller"
+import CountController from "./count_controller.js"
 application.register("count", CountController)
